552a3bd629f40daf58c8ce504c712facf48aa342
Ravshanbek Norboev <rnorboev00@gmail.com>
2017-02-11 22:32:53 -0600

    Added inc_text_links

--- Includes/inc_text_links.php
+++ Includes/inc_text_links.php
@@ -1,14 +1,29 @@
 <style type="text/css">
 	#text_links {
 		text-align:center;
-		background-color: #000;
-		color: #FFF;
-		font-size: 11px;
+		background-color: #333;
+		color: white;
+		font-size: 14px;
 		padding: 10px;
 		margin: 0 0 4px 0;
+
     }
+	#text_links a {
+		color:white;
+		text-decoration: none;
+
+	}
 </style>
 
 <div id="text_links">
-
+	<a href="#">Home Page</a> |
+	<a href="#">Site Layout</a> |
+	<a href="#">Control Structure</a> |
+	<a href="#">String Function</a> |
+	<a href="#">Web Forms</a> |
+	<a href="#">Midterm Assessment</a> |
+	<a href="#">State Information</a> |
+	<a href="#">User Templates</a> |
+	<a href="#">Final Project</a>
+	
 </div>
