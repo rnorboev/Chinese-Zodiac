6ea854c4913b5208d57a810c6ecd9bcabf98b77b
Ravshanbek Norboev <rnorboev00@gmail.com>
2017-03-20 20:34:27 -0500

    added new things

--- index.php
+++ index.php
@@ -14,30 +14,62 @@
 				include "Includes/inc_header.php" 
 			?>
 		</div>
-
 		<div>
 			<?php 
 				include "Includes/inc_text_links.php"
 			?>
 		</div>
-	<div id="content">
 		<div> <!-- This is the navigation placeholder --> 
 			<?php 
 				include "Includes/inc_button_nav.php"
 			?>
 		</div>
 
-		<div>
-			<?php 
-				include "Includes/inc_home.php"
-			?>
-		</div>
+	<div id="content">
+	<?php 
+		if (isset($_GET['page'])) {
+                switch ($_GET['page']) {
+                    case 'site_layout':
+                        include('Includes/inc_site_layout.php');
+                        break;
+                    case 'control_structures':
+                        include('Includes/inc_control_structures.php');
+                        break;
+                    case 'string_functions':
+                        include('Includes/inc_string_functions.php');
+                        break;
+                    case 'web_forms':
+                        include('Includes/inc_web_forms.php');
+                        break;
+                    case 'midterm_assessment':
+                        include('Includes/inc_midterm_assessment.php');
+                        break;
+                    case 'state_information':
+                        include('Includes/inc_state_information.php');
+                        break;
+                    case 'user_templates':
+                        include('Includes/inc_user_templates.php');
+                        break;
+                    case 'final_project':
+                        include('Includes/inc_final_project.php');
+                        break;
+                    case 'home_page':
+                    default:
+                        include('Includes/inc_home.php');
+                        break;
+                }
+            }
+            /* If no button has been selected, then display the default page*/
+            else { 
+                include('Includes/inc_home.php');
+            }
+	?>
+		
 	</div>
-		<div>
+			<div>
 			<?php 
 				include "Includes/inc_footer.php"
 			?>
 		</div>
-	</div>
 	</body>
 </html>
