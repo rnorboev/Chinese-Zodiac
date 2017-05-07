7c93763e2a560294122fbeb2e36d51d4f747d93a
Ravshanbek Norboev <rnorboev00@gmail.com>
2017-05-06 21:33:48 -0500

    fixed

--- Includes/inc_site_layout.php
+++ Includes/inc_site_layout.php
@@ -7,7 +7,7 @@
 <div class="dynamic">
 <h1>Site Layout</h1><br>
 <p>A dynamic Web template uses external files that contain, for example, CSS coding for formatting, styling and layout or PHP code for processing. Page elements commonly used in external files are text, graphics. The pages of a dynamic web template are connected in such a way that makes it easy to make modifications to the site. So instead of having to go into each HTML-coded file to make changes, the developer can go to these external modules and make the change once.</p><br>
-<p>Dynamic content can be displayed using a URL token so that it can change while the content and layout of the static sections stays the same. </p>
+<p>Dynamic content can be displayed using a URL token so that it can change while the content and layout of the static sections stays the same.<!--KEEP LOOKING FOR THE FLAG DO NOT WASTE YOUR TIME LOOK OTHER PLACES / PAGES--> </p>
 
 </div>
 </body>
